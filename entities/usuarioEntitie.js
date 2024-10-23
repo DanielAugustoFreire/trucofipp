453983af3045fe050ddd479c77dc21506e0e501f
Daniel Augusto <danielaugustosant@hotmail.com>
2024-10-23 18:35:34 -0300

    user atl

--- entities/usuarioEntitie.js
+++ entities/usuarioEntitie.js
@@ -9,14 +9,13 @@ export default class UsuarioEntitie extends BaseModel{
     #email;
     #senha;
     #nome;
-    #dataCadastrado;
 
     get id() { return this.#id; }                           set id(value) { this.#id = value; }
     get email() { return this.#email; }                     set email(value) { this.#email = value; }
     get senha() { return this.#senha; }                     set senha(value) { this.#senha = value; }
     get nome() { return this.#nome; }                       set nome(value) { this.#nome = value; }
 
-    constructor(id, email, senha, nome, dataCadastrado){
+    constructor(id, email, senha, nome){
         super();
         this.#id = id
         this.#email = email
