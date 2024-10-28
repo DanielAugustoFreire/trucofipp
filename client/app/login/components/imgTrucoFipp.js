410fb2edb772d11b7a3fc5e143a0cf1124496e8a
Daniel Augusto <danielaugustosant@hotmail.com>
2024-10-28 11:43:48 -0300

    logica do login add, so precisa arranjar uma forma de redirecionar o usuario para a pagina de /salas

--- client/app/login/components/imgTrucoFipp.js
+++ client/app/login/components/imgTrucoFipp.js
@@ -1,10 +1,10 @@
 export default function ImgTrucoFipp(){
 
 return (
-    <div class="col-md-9 col-lg-6 col-xl-5">
+    <div className="col-md-9 col-lg-6 col-xl-5">
     <img
       src="/img/LOGO_TRUCOFIPP.png"
-      class="img-fluid"
+      className="img-fluid"
       alt="Sample image"
     />
   </div>
--- client/app/login/registrar/components/registrar.js
+++ client/app/login/registrar/components/registrar.js
@@ -6,47 +6,47 @@ export default function Registar(){
 
     return (
         <form>
-        <div class="divider d-flex align-items-center my-4">
+        <div className="divider d-flex align-items-center my-4">
           <h2>REGISTRAR TRUCOFIPP ONLINE</h2>
         </div>
 
-        <div data-mdb-input-init class="form-outline mb-4">
+        <div data-mdb-input-init className="form-outline mb-4">
           <input
             type="email"
             id="form3Example3"
-            class="form-control form-control-lg"
+            className="form-control form-control-lg"
             placeholder="Digitge um email Valido"
           />
-          <label class="form-label" for="form3Example3">
+          <label className="form-label" for="form3Example3">
             Email
           </label>
         </div>
 
-        <div data-mdb-input-init class="form-outline mb-3">
+        <div data-mdb-input-init className="form-outline mb-3">
           <input
             type="password"
             id="form3Example4"
-            class="form-control form-control-lg"
+            className="form-control form-control-lg"
             placeholder="Digite Sua senha"
           />
-          <label class="form-label" for="form3Example4">
+          <label className="form-label" for="form3Example4">
             Senha
           </label>
         </div>
 
-        <div class="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center">
 
-          <a href="/cadastrar" class="text-body">
+          <a href="/cadastrar" className="text-body">
             Nao tem uma conta? Registre-se
           </a>
         </div>
 
-        <div class="text-center text-lg-start mt-4 pt-2">
+        <div className="text-center text-lg-start mt-4 pt-2">
           <button
             type="button"
             data-mdb-button-init
             data-mdb-ripple-init
-            class="btn btn-primary btn-lg"
+            className="btn btn-primary btn-lg"
             style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
           >
             Login
