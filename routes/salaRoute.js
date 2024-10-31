63530a6c2374c887b28412ae210f4de2ea053b71
Daniel AUgusto <danielaugustosant@hotmail.com>
2024-10-30 21:49:08 -0300

    desc certa para o swagger

--- routes/salaRoute.js
+++ routes/salaRoute.js
@@ -7,10 +7,13 @@ const router = express.Router();
 const ctrl = new salaController()
 
 router.get("/", (req, res) => {
+    // #swagger.tags = ['Salas']
+    // #swagger.summary = 'ListarSala
+    // #swagger.description = 'Essa rota lista as salas'
     ctrl.listarSalas(req,res);
 })
 router.post(`/`, (req, res) => {
-    // #swagger.tags = ['Sala']
+    // #swagger.tags = ['Salas']
     // #swagger.summary = 'Criar sala
     // #swagger.description = 'Essa rota cria uma sala'
     // #swagger.responses[201] = {
