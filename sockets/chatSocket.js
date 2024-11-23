d82c491002b6ed3c2edcf75d3664b6796b3f9193
DanielAugustoFreire <danielaugustosant@hotmail.com>
2024-11-23 14:20:37 -0300

    verificacao por id do jogo

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,4 +1,4 @@
-
+import JogoRepository from "../repositories/jogoRepository.js";
 
 
 export default function socket(io) {
@@ -6,11 +6,18 @@ export default function socket(io) {
 
   io.on('connection', (socket) => {
 
-    socket.on("ValidarPessoasSala", (msg) => {
-      if(msg.id == 1 ){
-        socket.emit("CarregarCartas", "Carregar Cartas");  // ENVIA MENSAGEM PARA O FRONTEND
-      }else{
-        socket.emit("Negado", "Acesso Negado");  // ENVIA MENSAGEM PARA O FRONTEND
+    socket.on("ValidarPessoasSala", async (msg) => {
+      try{
+        let joroRepo = new JogoRepository();
+        let result = await joroRepo.buscarJogoPorIdJogo(msg.id);
+        if(result.length > 0){
+          socket.emit("CarregarCartas", "Carregar Cartas");  // ENVIA MENSAGEM PARA O FRONTEND
+        }else{
+          socket.emit("Negado", "Acesso Negado");  // ENVIA MENSAGEM PARA O FRONTEND
+        }
+      }
+      catch(ex){
+        socket.emit("Negado", "Erro Interno");  // ENVIA MENSAGEM PARA O FRONTEND
       }
     })
   });
